38d3db7116933f478361ed2d7b5af959374fcda7
Andy <andnpatt@indiana.edu>
2015-11-09 17:21:05 -0500

    more changes to the scoring algorithm

--- qlearn.js
+++ qlearn.js
@@ -51,7 +51,6 @@ function run(){
     var endScore = game.score;
     var scoreDif = endScore - startScore ? endScore - startScore : 0;
     var reward = scoreDif / (1.25 *largestTile()) > 1 ? 1 : scoreDif / (largestTile() * 1.25);
-    console.log(reward);
     if(game.over) reward = 0;
 
 	var event = new Event('done' + brain.age);
